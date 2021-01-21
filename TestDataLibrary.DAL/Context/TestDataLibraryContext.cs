6487d3c759177babb90c26eb3c9d87b696b3150f
Serg <bsi_acs@mail.ru>
2021-01-21 10:21:48 +0300

    Добавьте файлы проекта.

---
+++ TestDataLibrary.DAL/Context/TestDataLibraryContext.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+using System.Text;
+using TestDataLibrary.Domain.Entities;
+
+namespace TestDataLibrary.DAL.Context
+{
+    public class TestDataLibraryContext : DbContext
+    {
+        public DbSet<User> Users { get; set; }
+
+        public TestDataLibraryContext([NotNullAttribute] DbContextOptions options) : base(options)
+        {
+            Database.EnsureCreated();
+        }
+
+
+    }
+}
