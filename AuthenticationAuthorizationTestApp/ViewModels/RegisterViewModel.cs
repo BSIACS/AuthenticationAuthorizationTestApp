75a604c9e8aa0b18f38f046b6de364c1e418e2d6
Serg <bsi_acs@mail.ru>
2021-01-21 16:19:31 +0300

    Добавлены классы LoginViewModel, RegisterViewModel

---
+++ AuthenticationAuthorizationTestApp/ViewModels/RegisterViewModel.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AuthenticationAuthorizationTestApp.ViewModels
+{
+    public class RegisterViewModel
+    {
+        [Required(ErrorMessage = "Не указан Email")]
+        public string Email { get; set; }
+
+        [Required(ErrorMessage = "Не указан пароль")]
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+
+        [Required(ErrorMessage = "Пароль введён не верно")]
+        [DataType(DataType.Password)]
+        public string ConfirmPassord { get; set; }
+    }
+}
