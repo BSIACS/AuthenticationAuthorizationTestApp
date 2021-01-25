261d7d54d56a6a42a3200a431ce991d319d8ced7
Serg <bsi_acs@mail.ru>
2021-01-25 16:26:02 +0300

    Модифицированно представление Privacy

--- AuthenticationAuthorizationTestApp/Controllers/HomeController.cs
+++ AuthenticationAuthorizationTestApp/Controllers/HomeController.cs
@@ -30,6 +30,9 @@ namespace AuthenticationAuthorizationTestApp.Controllers
         [Authorize]
         public IActionResult Privacy()
         {
+            ViewBag.Name = User.Identity.Name;
+            //HttpContext.User
+
             return View();
         }
 
